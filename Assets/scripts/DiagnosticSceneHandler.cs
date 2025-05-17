02b97b7f2629f7ce47e8d694014b3e3846594152
GOKUL RAJ E <gokulrajekambaram@gmail.com>
2025-05-17 12:45:13 +0530

    imuData implemented in unity

--- Assets/scripts/DiagnosticSceneHandler.cs
+++ Assets/scripts/DiagnosticSceneHandler.cs
@@ -27,8 +27,13 @@ public class DiagnosticSceneHandler : MonoBehaviour
                              "Theta2        : " + MarsComm.angleTwo.ToString() + "\n" +
                              "Theta3        : " + MarsComm.angleThree.ToString() + "\n" +
                              "Theta4        : " + MarsComm.angleFour.ToString() + "\n" +
+                             "IMUAng1       : " + MarsComm.imuAng1.ToString() + "\n" +
+                             "IMUAng2       : " + MarsComm.imuAng2.ToString() + "\n" +
+                             "IMUAng3       : " + MarsComm.imuAng3.ToString() + "\n" +
+                             "IMUAng4       : " + MarsComm.imuAng4.ToString() + "\n" +
                              "Force1        : " + MarsComm.forceOne.ToString() + "\n" +
                              "calibBtnState : " + MarsComm.calibBtnState.ToString() + "\n" +
+
                              "des1          : " + MarsComm.desOne.ToString() + "\n" +
                              "des2          : " + MarsComm.desTwo.ToString() + "\n" +
                              "des3          : " + MarsComm.desThree.ToString() + "\n" +
@@ -40,6 +45,10 @@ public class DiagnosticSceneHandler : MonoBehaviour
                              "lenLowerArm   : " + MarsComm.lenUpperArm.ToString() +"\n"+
                              "weight1       : " + MarsComm.lenUpperArm.ToString() + "\n" +
                              "weight2       : " + MarsComm.lenUpperArm.ToString() + "\n" +
+                             
+                             //$"IMU1         :  aX - {MarsComm.imu1aX.ToString()}    aY - {MarsComm.imu1aY.ToString()}   aZ - {MarsComm.imu1aZ}" + "\n" +
+                             //$"IMU2         :  aX - {MarsComm.imu2aX.ToString()}    aY - {MarsComm.imu2aY.ToString()}   aZ - {MarsComm.imu2aZ}" + "\n" +
+                             //$"IMU3         :  aX - {MarsComm.imu3aX.ToString()}    aY - {MarsComm.imu3aY.ToString()}   aZ - {MarsComm.imu3aZ}" + "\n" +
                              "Btn_st        : " + MarsComm.buttonState.ToString();
         
         UpdateTextBox.text = displayText;
