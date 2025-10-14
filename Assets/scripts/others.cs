5682b007157baaf052006494d2f76de9cf5d9886
GOKUL RAJ E <gokulrajekambaram@gmail.com>
2025-10-14 16:23:45 +0530

    Added cummulativeHits in gameOverPanel

--- Assets/scripts/others.cs
+++ Assets/scripts/others.cs
@@ -253,7 +253,7 @@ public class MarsUserData
         // AROM available.
         MarsArom arom = new MarsArom(movement, readFromFile: true);
         // Date string format: 13-10-2025 08:05:19
-        DateTime aromDate = DateTime.ParseExact(arom.datetime, DataManager.DATETIMEFORMAT, CultureInfo.InvariantCulture);
+        DateTime aromDate = DateTime.ParseExact(arom.datetime, DataManager.AROMDATEFORMAT, CultureInfo.InvariantCulture);
         TimeSpan duration = DateTime.Now - aromDate;
         return (int)duration.TotalDays;
     }
@@ -560,7 +560,7 @@ public class MarsArom
 
     private void initializeNewAssessment(string movementName)
     {
-        datetime = DateTime.Now.ToString(DataManager.DATETIMEFORMAT);
+        datetime = DateTime.Now.ToString(DataManager.AROMDATEFORMAT);
         movement = movementName;
         rawData = null;
         topRaw = UnityEngine.Vector2.zero;
