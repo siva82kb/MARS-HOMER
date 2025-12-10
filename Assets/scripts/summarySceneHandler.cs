d1234c381c66fdbbd309f2c705264ece4f93c6f5
GOKUL RAJ E <gokulrajekambaram@gmail.com>
2025-12-10 16:48:42 +0530

    HOMER-Final-Verison

--- Assets/scripts/summarySceneHandler.cs
+++ Assets/scripts/summarySceneHandler.cs
@@ -88,23 +88,23 @@ public class summarySceneHandler : MonoBehaviour
     public void exit()
     {
 
-        try
-        {
-            AppLogger.LogInfo("Disconnected form Mars And Application closed succesfully");
-            Application.Quit();
-            // Process.Start("shutdown", "/s /t 0");
-#if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-#endif
-            // Process.Start("shutdown", "/s /t 0");
-        }
-        catch (System.Exception ex)
-        {
-            Debug.LogError("Failed to shutdown: " + ex.Message);
-        }
-        //AppLogger.LogInfo("Disconnected form Mars And Switch scene to DataUploading");
+        //        try
+        //        {
+        //            AppLogger.LogInfo("Disconnected form Mars And Application closed succesfully");
+        //            Application.Quit();
+        //            // Process.Start("shutdown", "/s /t 0");
+        //#if UNITY_EDITOR
+        //            UnityEditor.EditorApplication.isPlaying = false;
+        //#endif
+        //            // Process.Start("shutdown", "/s /t 0");
+        //        }
+        //        catch (System.Exception ex)
+        //        {
+        //            Debug.LogError("Failed to shutdown: " + ex.Message);
+        //        }
+        AppLogger.LogInfo("Disconnected form Mars And Switch scene to DataUploading");
 
-        //SceneManager.LoadScene("DATAUPLOADING");
+        SceneManager.LoadScene("DATAUPLOADING");
 
     }
 
